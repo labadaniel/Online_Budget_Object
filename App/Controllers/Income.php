fdf681c8bf48723504f89fe574fb1cec0a685afc
Daniel <laba.daniel@gmail.com>
2019-11-25 21:26:43 +0100

    the incomes connected to db when user is putting the data

--- App/Controllers/Income.php
+++ App/Controllers/Income.php
@@ -12,6 +12,12 @@ use \App\Auth;
  */
 class Income extends \Core\Controller
 {
+    protected function before()
+    {
+      parent::before();
+
+      $this->user = Auth::getUser();
+    }
 
     /**
      * Show the index page
@@ -22,4 +28,12 @@ class Income extends \Core\Controller
     {
         View::renderTemplate('Income/show.html');
     }
+
+    public function addIncomeAction(){
+      if($this->user->addIncome($_POST, $this->user->id)){
+        View::renderTemplate('Home/index.html');
+      }
+    }
+
+
 }
